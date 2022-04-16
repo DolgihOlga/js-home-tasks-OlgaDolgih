2e648a034f5e9b13bdbb6e38f0904bf64fa9497d
Olga Dolgih <dolgiholgadol@gmail.com>
2022-04-16 19:50:36 +0300

    added task solution

---
+++ lesson-19-mvc/localStorage.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function TLocalStorage(name) {
+    var self = this,
+        pHash = {};
+    self.reset = function () {
+        pHash = JSON.parse(window.localStorage.getItem(name));
+        return pHash;
+    };
+    self.addValue = function (key, value) {
+        pHash[key] = value;
+        window.localStorage.setItem(name, JSON.stringify(pHash));
+    };
+    self.getValue = function (key) {
+        return pHash[key];
+    };
+    self.deleteValue = function (key) {
+        delete pHash[key];
+        window.localStorage.setItem(name, JSON.stringify(pHash));
+    };
+    self.getKeys = function () {
+        return (Object.keys(pHash));
+    };
+}
+
+
